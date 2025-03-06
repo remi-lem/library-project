19a495c579e9ee28ea18bf4910f507835f01e8bc
Rémi <contact@remilemaire.fr>
2025-03-06 19:22:21 +0100

    correction de l'erreur apres creation de compte

--- routes/web.php
+++ routes/web.php
@@ -71,10 +71,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    /*Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');*/
+    Route::get('/dashboard', function () {
+        return redirect('/dashboard-library');
+    });
 });
-/*Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');*/
+
 
 // SNEAT
 Route::middleware(['auth', AdminMiddleware::class])->group(function () {
